ab236ff628dc7c28a08a2c6cb23aeb2c34bba926
Eric Gan <ericgan@mit.edu>
2015-05-03 20:39:27 -0400

    lecture/class fixes

--- public/javascripts/classLec.js
+++ public/javascripts/classLec.js
@@ -7,20 +7,22 @@ $(document).on('click', '.edit', function (e) {
 	e.preventDefault();
 	var parent = $(this).parent().parent().parent();
 	var content = $(parent).find('.className')[0].textContent;
-	console.log(content)
 
 	editCard('classEdit',{name: content}, parent);
-	$('.focusQuestion').focus();
+	var children = parent.children();
+	var focus = children[0].children[1];
+	focus.focus();
 });
 
 $(document).on('click', '.editLec', function (e) {
 	e.preventDefault();
 	var parent = $(this).parent().parent().parent();
 	var content = $(parent).find('.className')[0].textContent;
-	console.log(content)
 
 	editCard('lecEdit',{name: content}, parent);
-	$('.focusQuestion').focus();
+	var children = parent.children();
+	var focus = children[0].children[1];
+	focus.focus();
 });
 
 $(document).on('click', '.cancel', function (e) {
@@ -47,7 +49,7 @@ $(document).on('click', '.save', function (e) {
 	var content = $(parent).find("input[type=text]")[0];
 	var className = $(content).val();
 	var hidden = $(parent).find("input[type=hidden]")[0];
-	var oldName = $(content).val();
+	var oldName = $(hidden).val();
 
 	console.log(className.trim().length)
 	if (className.trim().length > 0) {
@@ -63,7 +65,7 @@ $(document).on('click', '.saveLec', function (e) {
 	var content = $(parent).find("input[type=text]")[0];
 	var className = $(content).val();
 	var hidden = $(parent).find("input[type=hidden]")[0];
-	var oldName = $(content).val();
+	var oldName = $(hidden).val();
 
 	console.log(className.trim().length)
 	if (className.trim().length > 0) {
